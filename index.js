d8715b378613fa015b6425119d41bd78b5c39280
Bernadette92 <bernadettehoogervorst@gmail.com>
2021-03-22 19:03:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const name = prompt( "Welkom! Wat is je naam?");
+console.log ( "Hey " + name);
+
+for (i=4; i>-1; i--){
+const gok = prompt ( "Voer een nummer in van 0 tot 25 om te beginnen met raden...");
+
+const inputCijfer = function (){
+const inputCijfer = math.round(math.random()*25) + 1;
+}
+
+ if (gok == inputCijfer) {
+alert("Gefeliciteerd je hebt gewonnen");
+break;
+ } else {
+   alert("Dat is niet correct, je hebt "+ (i) + " pogingen over.")
+   if (i === 0){
+    console.log ("Dag " +name+ ". Tot de volgende keer");
+
+   } else {
+     console.log ("Probeer het nog een keer.");
+   }
+    }
+}
+
+
+
+
+
+
